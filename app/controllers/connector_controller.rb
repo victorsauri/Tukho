673402993ffae95b643af8f92b7319a30f7874f1
victorsauri <vssantacreu@gmail.com>
2014-11-24 16:24:38 +0000

    new relation between user and event model stablished. Removal of connector model and miration

--- app/controllers/connector_controller.rb
+++
@@ -1,2 +0,0 @@
-class ConnectorController < ApplicationController
-end
--- db/migrate/20141124130406_create_connectors.rb
+++
@@ -1,9 +0,0 @@
-class CreateConnectors < ActiveRecord::Migration
-  def change
-    create_table :connectors do |t|
-    	t.integer :user_id
-    	t.integer :event_id
-      t.timestamps
-    end
-  end
-end
