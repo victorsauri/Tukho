d3c121629890e5d48be71b2ee56376c7e78e28c5
victorsauri <vssantacreu@gmail.com>
2014-11-24 13:13:02 +0000

    table properties stablish for each model

--- db/migrate/20141124130406_create_connectors.rb
+++ db/migrate/20141124130406_create_connectors.rb
@@ -1,7 +1,8 @@
 class CreateConnectors < ActiveRecord::Migration
   def change
     create_table :connectors do |t|
-
+    	t.integer :user_id
+    	t.integer :event_id
       t.timestamps
     end
   end
--- db/migrate/20141124130431_create_events.rb
+++ db/migrate/20141124130431_create_events.rb
@@ -1,7 +1,14 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-
+    	t.string :name
+    	t.string :description
+    	t.datetime :date
+    	t.string :photo
+    	t.string :location
+    	t.datetime :starting_time
+    	t.datetime :finishing_time
+    	t.integer :num_people
       t.timestamps
     end
   end
