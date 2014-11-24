dab84d076851749345cc11909503776bb1b9b1fc
victorsauri <vssantacreu@gmail.com>
2014-11-24 21:51:27 +0000

    Random generation of Users and Events in the database to start with

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 require 'faker'
 
+25.times do
+	User.create(name: Faker::Name.name, description: Faker::Lorem.sentence, 
+		color: Faker::Commerce.color, photo: Faker::Avatar.image, privacy: false)
+end
+50.times do
+	event = Event.create(name: Faker::Name.name, description: Faker::Lorem.sentence, date: Faker::Date.forward(20),
+	photo: Faker::Avatar.image, location: Faker::Address.street_address, starting_time: Faker::Time.between(1.days.ago, Time.now, :evening), 
+	starting_time: Faker::Time.between(1.days.ago, Time.now, :midnight), num_people: Faker::Number.number(100))
+	
+	offset = rand(User.count)
+	event.users << User.offset(offset).first
+end
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
