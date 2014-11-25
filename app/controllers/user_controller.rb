6fefb0980e43e9c9b82da90131f36d64829c1323
victorsauri <vssantacreu@gmail.com>
2014-11-25 10:49:00 +0000

    basic formulaire created in view new of user model

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,18 @@
 class UserController < ApplicationController
+
 	def home
 	end
+
 	def index
 	end
+
 	def new
 		@user = User.new
+	end
+
+	private
+
+	def get_random_users_for_seed
+  find(:all).sample(5)
 	end
 end
