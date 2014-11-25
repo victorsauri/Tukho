337e25d2cf3ac87744fa687cd3fd17f4a08009a6
victorsauri <vssantacreu@gmail.com>
2014-11-25 22:19:55 +0000

    partial view form created and rendered. Implementation of methods edit, update and destroy in user model.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,7 +5,7 @@ class UserController < ApplicationController
 	end
 
 	def index
-		@user = User.all
+		@user = User.order(created_at: :desc).limit(10)
 	end
 
 	def new
@@ -19,7 +19,6 @@ class UserController < ApplicationController
 	end
 
 	def create
-
 		@user = User.find_by(name: params[:user][:name])
     unless @user
 	    @user = User.new(name: params[:user][:name], description: params[:user][:description],
@@ -32,7 +31,36 @@ class UserController < ApplicationController
 	  end
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+
+		if @user.update_attributes user_params
+	      redirect_to action: :show, controller: 'user', user_id: @user.id
+	    else
+	      render 'edit'
+    end
+   end
+
+  def destroy
+      @user = User.find(params[:id])
+
+      if @user.destroy
+      	# flash[:confirmation] = "Entry deleted successfully"
+      	redirect_to action: :index
+      else
+     		redirect_to :back
+  	end
+  end
+
 	private
+
+	def user_params
+   	params.require(:user).permit(:name, :description, :color, :privacy)
+  end
 
 	# def get_random_users_for_seed
  #  	find(:all).sample(5)
