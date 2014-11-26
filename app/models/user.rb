9ebf80b7497eebbcd8822302a4617943384f5a55
victorsauri <vssantacreu@gmail.com>
2014-11-26 17:47:32 +0000

    validations formats created for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 	has_and_belongs_to_many :events
 
-	validates :name, presence: true
-	validates :description, presence: true
+	validates :name, presence: true, format: {with: /\A[\w\s*]+\Z/}
+	validates :description, presence: true, format: {with: /\A[\w\s*]+\Z/}
 	validates :color, presence: true
 	# validates :photo, presence: true
+	validates_inclusion_of :privacy, :in => [true, false]
 end
