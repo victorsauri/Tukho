850db598b6c2cfd6d0091fa3ac7225eddd3d38d0
victorsauri <vssantacreu@gmail.com>
2014-11-26 22:41:05 +0000

    validation of model user edited

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,6 @@ class User < ActiveRecord::Base
 
 	validates :name, presence: true, format: {with: /\A[\w\s*]+\Z/}
 	validates :description, presence: true, format: {with: /\A[\w\s*]+\Z/}
-	# validates :color, presence: true
-	# validates :photo, presence: true
 	validates_inclusion_of :privacy, :in => [true, false]
-
-# def get_random_users
-#   	find(:all).sample
-# end
-
-
+	# validates :photo, presence: true
 end
