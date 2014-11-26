01505b9ae63ec64d63dff0d43b026c51b4c31df3
victorsauri <vssantacreu@gmail.com>
2014-11-26 20:43:19 +0000

    association branch and removal of table user_event

--- db/migrate/20141124164912_create_users_events.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsersEvents < ActiveRecord::Migration
-  def change
-    create_table :users_events, id: false do |t|
-      t.belongs_to :user
-      t.belongs_to :event
-    end
-  end
-end
