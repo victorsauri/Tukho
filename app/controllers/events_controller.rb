f508cab92394a656961e9c3ef16ffab5a5e18668
victorsauri <vssantacreu@gmail.com>
2014-11-29 16:13:01 +0000

    index method defined in events controller. Index view created showing fake events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
+	def index
+			@event = Event.order(created_at: :desc).limit(11)
+	end
 end
 
-# event.connectors.each do |connector|
-# 	connector.color
-# end
