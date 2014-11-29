42e8f285605dcaaf4f40bf7ddbf3abcada5c7d8f
victorsauri <vssantacreu@gmail.com>
2014-11-29 19:01:13 +0000

    defining method edit of event to show properties

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,15 @@
 class EventsController < ApplicationController
+	helper_method :event_params
 	def index
 		@event = Event.order(created_at: :desc).limit(8)
 	end
 	def edit
-		@event = Event.find params[:id]
+		@event = Event.find(name: params[:name], description: params[:description],
+	    location: params[:location], date: params[:date])
 	end
+ private
+	def event_params
+   	params.require(:event).permit(:name, :description, :location, :date, :starting_time, :finishing_time)
+  end
 end
 
