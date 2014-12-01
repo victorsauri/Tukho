b0210594e68ceb248f50ab03897ffbc5ee909afb
victorsauri <vssantacreu@gmail.com>
2014-12-01 23:32:37 +0000

    additional methods and attributes created for the choose_color method in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,9 @@
 class EventsController < ApplicationController
 	helper_method :event_params
 	helper_method :connector_params
+	helper_method :current_user
+	attr_accessible :color
+	# before_action :authenticate_user!
 
 	def index
 		@event = Event.order(created_at: :desc).limit(8)
@@ -12,12 +15,11 @@ class EventsController < ApplicationController
 	 	end
 	end
 	def choose_color
-		@connector = current_user.connectors.where(event_id:params[:id])
-		@connector.update(color: params[:color])
+	 	@connector = current_user.connectors.where(event_id:params[:id]).first
+		@connector.attributes connector_params
+		@connector.save
 		redirect_to :back
 	end
-
-
 
 	private
 	def event_params
@@ -27,14 +29,3 @@ class EventsController < ApplicationController
 	 	params.require(:connector).permit(:color, :user_id, :event_id)
   end
 end
-
- # Event.users << User.find params[:id]
-
- 	# def edit
-	# 	@connector = Event.find_by(color: params[:connector][:color], user_id: params[:connector][:user_id],
-	#     even_id: params[:connector]	[:even_id])
-	# end
- # private
-	# def connector_params
- #   	params.require(:connector).permit(:color, :user_id, :event_id)
- #  end
