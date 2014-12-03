0db94c6046d9db6dae2770d311fe1eb4b461f3f5
victorsauri <vssantacreu@gmail.com>
2014-12-03 08:40:42 +0000

    display speedometer event list, with random seted values

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,22 +16,3 @@
 //= require_tree .
 
 
-var opts = {
-  lines: 12, // The number of lines to draw
-  angle: 0.15, // The length of each line
-  lineWidth: 0.44, // The line thickness
-  pointer: {
-    length: 0.9, // The radius of the inner circle
-    strokeWidth: 0.035, // The rotation offset
-    color: '#000000' // Fill color
-  },
-  limitMax: 'false',   // If true, the pointer will not go past the end of the gauge
-  percentColors: [[0.0, "#a9d70b" ],[0.49999, "#a9d70b" ],[0.50, "#f9c802"],[0.79999, "#f9c802"],[0.825, "#ff0000"], [1.0, "#ff0000"]],
-  strokeColor: '#E0E0E0',   // to see which ones work best for you
-  generateGradient: true
-};
-var target = document.getElementsByClassName('Gauge'); // your canvas element
-var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
-gauge.maxValue = document.getElementsByClassName('count_all').value; // set max gauge value
-gauge.animationSpeed = 32; // set animation speed (32 is default value)
-gauge.set(5); // set actual maxValue
